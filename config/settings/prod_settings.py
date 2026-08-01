1e06882224e70bb01c3b3fb4423e1d539c5c4b78
NnaemekaAlgorithim <nnaemekanwodo625@gmail.com>
2026-08-01 13:02:21 +0100

    added the requirements correctly

--- config/settings/prod_settings.py
+++ config/settings/prod_settings.py
@@ -11,3 +11,8 @@ STATIC_URL = '/cotn/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/cotn/media/'
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
+
+# nginx strips the /cotn prefix before forwarding, so urlpatterns stay unprefixed;
+# this makes reverse()/build_absolute_uri()/pagination/schema links re-add it correctly.
+FORCE_SCRIPT_NAME = '/cotn'
+
